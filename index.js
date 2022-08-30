7f26ce9807bd90f87199a67bc5e21d9792789124
Ruan Esterhuyse <ruan.esterhuyse@gmail.com>
2022-08-30 18:25:07 -0400

    add

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ let header = document.getElementById("header");
 console.log(header);
 header.addEventListener("click", changeColor);
 
-function changeColor(){
-header.classList.add("myheader");
-header.style.color ="white";
+function changeColor(event){
+event.target.classList.toggle("myheader");
 
 }
-
-
 
